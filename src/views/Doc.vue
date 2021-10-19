c033677dea679a086d325ecfcad14fb217600c54
untilthecore <814898862@qq.com>
2021-10-19 17:38:09 +0800

    <TopNav>封装完成

---
+++ src/views/Doc.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <TopNav />
+    <div class="content">
+      <aside>边栏</aside>
+      <main>主内容</main>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import TopNav from '@/components/TopNav.vue';
+</script>
---
+++ src/views/Home.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    <TopNav />
+    <div class="banner">
+      <h1>轱辘UI</h1>
+      <h2>一个厉害的 UI 框架</h2>
+      <p class="actions">
+        <a>GitHub</a>
+        <a>开始</a>
+      </p>
+    </div>
+  </div>
+</template>
+<script setup lang="ts">
+import TopNav from '@/components/TopNav.vue';
+</script>
+<style lang="scss" scoped>
+</style>
