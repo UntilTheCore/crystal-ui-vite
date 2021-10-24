f063088eb925c112712a9cbcb84c64a8c8029637
untilthecore <814898862@qq.com>
2021-10-25 01:15:56 +0800

    实现<Switch />组件点击切换功能

--- src/lib/Switch.vue
+++ src/lib/Switch.vue
@@ -1,38 +1,49 @@
 <template>
-  <div class="switch">
-    <button><span></span></button>
-  </div>
+  <button
+    :class="{checked}"
+    @click.stop.capture="toggle"
+  >
+    <span></span>
+  </button>
 </template>
 
 <script setup lang="ts">
+import { ref } from "vue";
+
+const checked = ref( false );
+const toggle = () => {
+  checked.value = !checked.value;
+  console.log( checked.value );
+};
 </script>
 
 <style lang="scss" scoped>
-.switch {
-  $h: 22px;
-  $h2: $h - 4px;
-  background-color: #ccc;
+$h: 22px;
+$h2: $h - 4px;
 
-  button {
-    height: $h;
-    width: $h*2;
-    border: none;
+button {
+  height: $h;
+  width: $h*2;
+  border: none;
+  background: gray;
+  border-radius: $h/2;
+  position: relative;
+
+  span {
+    position: absolute;
+    top: 2px;
+    left: 2px;
+    height: $h2;
+    width: $h2;
+    background: white;
+    border-radius: $h2 / 2;
+    transition: all .3s;
+  }
+
+  &.checked {
     background: #42b983;
-    border-radius: $h/2;
-    position: relative;
 
-    span {
-      position: absolute;
-      top: 2px;
-      left: 2px;
-      height: $h2;
-      width: $h2;
-      background: white;
-      border-radius: $h2 / 2;
-      transition: all .3s;
-    }
-
-    &:hover > span {
+    > span {
       left: calc(100% - #{$h2} - 2px);
     }
   }
