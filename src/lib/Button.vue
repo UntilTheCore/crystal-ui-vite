9297a0b08a341c52606aea379331ce7ebe9897e1
untilthecore <814898862@qq.com>
2021-10-26 00:55:43 +0800

    <Button />组件绑定$attrs

--- src/lib/Button.vue
+++ src/lib/Button.vue
@@ -1,12 +1,21 @@
 <template>
-  <div class="cs-button">
-    <button>
-      <slot />
-    </button>
-  </div>
+  <button class="cs-button" v-bind="attrs">
+    <slot />
+  </button>
 </template>
 
+<script lang="ts">
+export default {
+  // 取消默认绑定到根节点
+  inheritAttrs: false
+};
+</script>
+
 <script setup lang="ts">
+import { useAttrs } from "vue";
+
+const attrs = useAttrs();
+console.log(attrs);
 </script>
 
 <style lang="scss" scoped>
--- src/views/doc/ButtonDemo.vue
+++ src/views/doc/ButtonDemo.vue
@@ -1,11 +1,25 @@
 <template>
   <div class="button-demo">
-    <Button>Default</Button>
+    <Button
+      size="large"
+      @click="onClick"
+      @mouseover="onMouseOver"
+    >
+      Default
+    </Button>
   </div>
 </template>
 
 <script setup lang="ts">
 import Button from "@/lib/Button.vue";
+
+const onClick = () => {
+  console.log( "click" );
+};
+
+const onMouseOver = () => {
+  console.log( "mouse over" );
+};
 </script>
 
 <style lang="scss" scoped>
