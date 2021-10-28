22f2db17c9cff8103d8a585e326d7c29ae798704
untilthecore <814898862@qq.com>
2021-10-28 10:53:51 +0800

    v-model控制<Dialog />显示

--- src/lib/Dialog.vue
+++ src/lib/Dialog.vue
@@ -1,28 +1,113 @@
 <template>
-  <div class="cs-dialog">
-    <section class="cs-dialog__title">
-      <p class="cs-dialog__title-content">
-        标题
-      </p>
-      <i>x</i>
-    </section>
-    <section class="cs-dialog__main">
-      <p>第一行</p>
-      <p>第二行</p>
-    </section>
-    <section class="cs-dialog__footer">
-      <Button>确定</Button>
-      <Button>取消</Button>
-    </section>
-  </div>
+  <template v-if="modelValue">
+    <div class="cs-dialog__overlay"></div>
+    <div class="cs-dialog-wrapper">
+      <div class="cs-dialog">
+        <section class="cs-dialog__header">
+          <p class="cs-dialog__header-title">标题</p>
+          <i class="cs-dialog__header-close"></i>
+        </section>
+        <section class="cs-dialog__main">
+          <p>第一行</p>
+          <p>第二行</p>
+        </section>
+        <section class="cs-dialog__footer">
+          <Button level="main">确定</Button>
+          <Button>取消</Button>
+        </section>
+      </div>
+    </div>
+  </template>
 </template>
 
 <script setup lang="ts">
 import Button from "./Button.vue";
+
+type Props = {
+  modelValue: boolean,
+}
+
+defineEmits(['update:modelValue'])
+
+withDefaults(defineProps<Props>(), {
+  modelValue: false,
+})
+
 </script>
 
 <style lang="scss">
+$radius: 4px;
+$border-color: #d9d9d9;
+
 .cs-dialog {
-  border: 1px solid skyblue;
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+
+  &__overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+  &__header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+
+    &-close {
+      position: relative;
+      display: inline-block;
+      width: 16px;
+      height: 16px;
+      cursor: pointer;
+
+      &::before,
+      &::after {
+        content: "";
+        position: absolute;
+        height: 1px;
+        background: black;
+        width: 100%;
+        top: 50%;
+        left: 50%;
+      }
+
+      &::before {
+        transform: translate(-50%, -50%) rotate(-45deg);
+      }
+
+      &::after {
+        transform: translate(-50%, -50%) rotate(45deg);
+      }
+    }
+  }
+
+  &__main {
+    padding: 12px 16px;
+  }
+
+  &__footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
 }
 </style>
--- src/views/doc/DialogDemo.vue
+++ src/views/doc/DialogDemo.vue
@@ -1,16 +1,22 @@
 <template>
   <div class="dialog-demo">
     <div>dialog示例</div>
-    <Dialog />
+    <Button @click="data.visible = true">打开dialog</Button>
+    <Dialog v-model="data.visible" />
   </div>
 </template>
 
 <script setup lang="ts">
+import { ref } from 'vue';
 import Dialog from '@/lib/Dialog.vue';
+import Button from '@/lib/Button.vue';
+
+const data = ref({
+  visible: false
+})
 </script>
 
 <style lang="scss" scoped>
 .dialog-demo {
-  border: 1px solid skyblue;
 }
 </style>
