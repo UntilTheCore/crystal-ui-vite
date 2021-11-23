4b262f71eaf609c1723b06ac87d3cf3d7e50fb06
untilthecore <814898862@qq.com>
2021-11-23 14:54:48 +0800

    demo工程创建使用<Icon />

---
+++ src/components/Icon.vue
@@ -0,0 +1,29 @@
+<template>
+  <svg class="icon" aria-hidden="true" :style="{ width, height }">
+    <use :xlink:href="`#icon-${name}`" />
+  </svg>
+</template>
+
+<script setup lang="ts">
+type Props = {
+  name: string,
+  width?: string,
+  height?: string,
+}
+const props = withDefaults(defineProps<Props>(), {
+  width: '1em',
+  height: '1em',
+});
+
+</script>
+
+<style lang="scss" scoped>
+.icon {
+  width: 1em;
+  height: 1em;
+  vertical-align: -0.15em;
+  fill: currentColor;
+  overflow: hidden;
+  color: #fff;
+}
+</style>
