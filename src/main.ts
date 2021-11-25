68add27872124d8ff3e1091ee80d5e7a99603612
untilthecore <814898862@qq.com>
2021-11-25 17:49:32 +0800

    支持markdown语法格式

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,8 @@ import "@/assets/style/index.scss";
 import "@/lib/style/crystal.scss";
 import App from "@/App.vue";
 import router from "@/router";
+import "github-markdown-css";
 
-const app = createApp( App );
-app.use( router );
-app.mount( "#app" );
+const app = createApp(App);
+app.use(router);
+app.mount("#app");
