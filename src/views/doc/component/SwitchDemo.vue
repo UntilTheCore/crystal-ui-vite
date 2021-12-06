084eeabb6a568dd590ff0745e126a286dceed3fc
untilthecore <814898862@qq.com>
2021-12-06 10:40:05 +0800

    修改 <SwitchDemo /> 标题内容

--- src/views/doc/component/SwitchDemo.vue
+++ src/views/doc/component/SwitchDemo.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
-    <h1>Switch 组件示例</h1>
+    <h1>Switch 开关</h1>
+    <p>表示两种相互对立的状态间的切换，多用于触发「开/关」。</p>
     <Demo :component="Switch1Demo" />
     <Demo :component="Switch2Demo" />
   </div>
