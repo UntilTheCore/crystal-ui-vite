d4bab2e861d257b0c38b7d54db09405784eb6723
untilthecore <814898862@qq.com>
2021-12-07 21:26:51 +0800

    解决顶部导航栏被遮挡的问题

--- src/components/Aside.vue
+++ src/components/Aside.vue
@@ -65,7 +65,7 @@ aside {
   width: var(--asidebar-width);
   height: 100vh;
   padding: calc(var(--navbar-height) + 10px) 0 16px;
-  z-index: 999;
+  z-index: 1;
 
   > h2 {
     margin-bottom: 4px;
